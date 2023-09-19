0a96e61a6a8f71eee997e26a8884c6bdde2491ce
Ayush <ajha06683@gmail.com>
2023-09-19 19:09:54 +0530

    2nd commit

--- calculator/calci.js
+++ calculator/calci.js
@@ -1,15 +1,13 @@
-function appendToBar(value) 
-{
-            document.getElementById("bar").value += value;
+
+function appendToBar(value) {
+    document.getElementById("bar").value += value;
 }
 
-function clearBar() 
-{
-            document.getElementById("bar").value = "";
+function clearBar() {
+    document.getElementById("bar").value = "";
 }
 
-function calculate() 
-{
+function calculate() {
     try {
         const result = eval(document.getElementById("bar").value);
         document.getElementById("bar").value = result;
@@ -17,3 +15,29 @@ function calculate()
         document.getElementById("bar").value = "Error";
     }
 }
+
+function calculateExponent() {
+    const expression = document.getElementById("bar").value;
+    const operand = parseFloat(expression);
+    if (!isNaN(operand)) {
+        const result = Math.pow(operand, 2);;
+        document.getElementById("bar").value = result;
+    } else {
+        document.getElementById("bar").value = "Invalid Input";
+    }
+}
+
+
+
+function calculateSquareRoot() {
+    const expression = document.getElementById("bar").value;
+    const operand = parseFloat(expression);
+    if (!isNaN(operand)) {
+        const result = Math.sqrt(operand);
+        document.getElementById("bar").value = result;
+    } else {
+        document.getElementById("bar").value = "Invalid Input";
+    }
+}
+
+
